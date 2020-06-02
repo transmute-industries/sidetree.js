6b88644da3a54a0c488d2a680af8b350261c9752
gjgd <gjgd@protonmail.com>
2020-06-02 12:31:00 +0200

    test: add test create operation with json patch

---
+++ packages/sidetree.js/test/create.test.ts
@@ -0,0 +1,24 @@
+import {
+  generateCreateOperation,
+  ICreateOperationData,
+} from './operations_helper';
+import DidStatePatch from '../src/DidStatePatch';
+
+// TODO: describe jest error
+// TODO: prettier
+// TODO: test negative test
+// TODO: test for arbitrary object
+describe('Create operation', () => {
+  let createOperationData: ICreateOperationData;
+
+  it('should contain a delta with valid ietf json patch', async () => {
+    createOperationData = await generateCreateOperation();
+    const delta = createOperationData.createOperation.delta!;
+    expect(delta).toBeDefined();
+    expect(delta.updateCommitment).toBeDefined();
+    expect(delta.patches).toBeDefined();
+    delta.patches.forEach(patch => {
+      expect(() => DidStatePatch.validatePatch(patch)).not.toThrow();
+    });
+  });
+});
