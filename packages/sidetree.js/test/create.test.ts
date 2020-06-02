1eb129c2a74340ca4c435f0e85823deab63b77f9
gjgd <gjgd@protonmail.com>
2020-06-02 15:00:53 +0200

    test: arbitrary did document passed to the create operation

--- packages/sidetree.js/test/create.test.ts
+++ packages/sidetree.js/test/create.test.ts
@@ -1,8 +1,13 @@
 import {
+  generateCommitRevealPair,
+  generateKeyPair,
+  generateCreateOperationRequest,
   generateCreateOperation,
   ICreateOperationData,
 } from './operations_helper';
 import DocumentComposer from '../src/DocumentComposer';
+import CreateOperation from '../src/CreateOperation';
+import Jwk from '../src/util/Jwk';
 
 describe('Create operation', () => {
   let createOperationData: ICreateOperationData;
@@ -16,5 +21,30 @@ describe('Create operation', () => {
     delta.patches.forEach((patch) => {
       expect(() => DocumentComposer.validatePatch(patch)).not.toThrow();
     });
+  });
+
+  it('should be able to pass arbitrary did document', async () => {
+    const signingKeyId = 'signingKey';
+    const [recoveryPublicKey, ,] = await Jwk.generateEs256kKeyPair();
+    const [signingPublicKey] = await generateKeyPair(signingKeyId);
+    const [, nextUpdateCommitmentHash] = generateCommitRevealPair();
+    const arbitraryDocument = {
+      arbitrary: true,
+      nested: {
+        property: 'work',
+      },
+      publicKeys: [signingPublicKey],
+    };
+    const operationRequest = await generateCreateOperationRequest(
+      recoveryPublicKey,
+      nextUpdateCommitmentHash,
+      arbitraryDocument
+    );
+    const operationBuffer = Buffer.from(JSON.stringify(operationRequest));
+    const createOperation = await CreateOperation.parse(operationBuffer);
+    expect(createOperation).toBeDefined();
+    const patches = createOperation.delta!.patches;
+    const didDocument = DocumentComposer.applyPatches({}, patches);
+    expect(didDocument).toEqual(arbitraryDocument);
   });
 });
