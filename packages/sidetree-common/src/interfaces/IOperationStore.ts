e77949ac5c38c17f75496dd6912c3f8d10e4f147
gjgd <gjgd@protonmail.com>
2020-06-03 16:25:52 +0200

    feat: add missing interface and model to sidetree-common

---
+++ packages/sidetree-common/src/interfaces/IOperationStore.ts
@@ -0,0 +1,30 @@
+import AnchoredOperationModel from '../models/AnchoredOperationModel';
+
+/**
+ * An abstraction of a complete store for operations exposing methods to
+ * put and get operations.
+ */
+export default interface IOperationStore {
+
+  /**
+   * Stores a batch of operations
+   * @param operations The list of operations to be stored, where the key of the map is the DID unique suffix.
+   */
+  put (operations: AnchoredOperationModel[]): Promise<void>;
+
+  /**
+   * Gets all operations of the given DID unique suffix in ascending chronological order.
+   */
+  get (didUniqueSuffix: string): Promise<AnchoredOperationModel[]>;
+
+  /**
+   * Deletes all operations with transaction number greater than the
+   * provided parameter.
+   */
+  delete (transactionNumber?: number): Promise<void>;
+
+  /**
+   * Deletes all the operations of the given DID earlier than the specified operation.
+   */
+  deleteUpdatesEarlierThan (didUniqueSuffix: string, transactionNumber: number, operationIndex: number): Promise<void>;
+}
