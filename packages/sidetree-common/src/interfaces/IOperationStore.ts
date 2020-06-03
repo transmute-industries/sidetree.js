99898dd8205a3a4990a65db8beb0c6f70aeeae7e
gjgd <gjgd@protonmail.com>
2020-06-04 00:03:58 +0200

    chore: update IOperationStore interface

--- packages/sidetree-common/src/interfaces/IOperationStore.ts
+++ packages/sidetree-common/src/interfaces/IOperationStore.ts
@@ -5,26 +5,31 @@ import AnchoredOperationModel from '../models/AnchoredOperationModel';
  * put and get operations.
  */
 export default interface IOperationStore {
-
   /**
    * Stores a batch of operations
    * @param operations The list of operations to be stored, where the key of the map is the DID unique suffix.
    */
-  put (operations: AnchoredOperationModel[]): Promise<void>;
+  put(operations: AnchoredOperationModel[]): Promise<void>;
 
   /**
    * Gets all operations of the given DID unique suffix in ascending chronological order.
    */
-  get (didUniqueSuffix: string): Promise<AnchoredOperationModel[]>;
+  get(didUniqueSuffix: string): Promise<AnchoredOperationModel[]>;
 
   /**
    * Deletes all operations with transaction number greater than the
    * provided parameter.
    */
-  delete (transactionNumber?: number): Promise<void>;
+  delete(transactionNumber?: number): Promise<void>;
 
   /**
    * Deletes all the operations of the given DID earlier than the specified operation.
    */
-  deleteUpdatesEarlierThan (didUniqueSuffix: string, transactionNumber: number, operationIndex: number): Promise<void>;
+  deleteUpdatesEarlierThan(
+    didUniqueSuffix: string,
+    transactionNumber: number,
+    operationIndex: number
+  ): Promise<void>;
+
+  close(): Promise<void>;
 }
