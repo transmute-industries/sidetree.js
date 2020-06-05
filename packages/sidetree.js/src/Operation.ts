e94d5d52cc3b18d778b4e608760f57755d97c288
gjgd <gjgd@protonmail.com>
2020-06-05 13:59:42 +0200

    feat: add parse method to Operation class

--- packages/sidetree.js/src/Operation.ts
+++ packages/sidetree.js/src/Operation.ts
@@ -1,10 +1,16 @@
 import DeltaModel from '@sidetree/common/src/models/DeltaModel';
+import OperationModel from '@sidetree/common/src/models/OperationModel';
+import OperationType from '@sidetree/common/src/enums/OperationType';
 import DocumentComposer from './DocumentComposer';
 import Encoder from './util/Encoder';
 import ErrorCode from '@sidetree/common/src/errors/ErrorCode';
 import JsonAsync from './util/JsonAsync';
 import Multihash from './util/Multihash';
 import SidetreeError from '@sidetree/common/src/errors/SidetreeError';
+import CreateOperation from './CreateOperation';
+import UpdateOperation from './UpdateOperation';
+import RecoverOperation from './RecoverOperation';
+import DeactivateOperation from './DeactivateOperation';
 
 /**
  * A class that contains Sidetree operation utility methods.
@@ -12,6 +18,45 @@ import SidetreeError from '@sidetree/common/src/errors/SidetreeError';
 export default class Operation {
   /** Maximum allowed encoded reveal value string length. */
   public static readonly maxEncodedRevealValueLength = 50;
+
+  /**
+   * Parses the given buffer into an `OperationModel`.
+   */
+  public static async parse(operationBuffer: Buffer): Promise<OperationModel> {
+    // Parse request buffer into a JS object.
+    const operationJsonString = operationBuffer.toString();
+    const operationObject = JSON.parse(operationJsonString);
+    const operationType = operationObject.type;
+    const isAnchorFileMode = false;
+
+    if (operationType === OperationType.Create) {
+      return CreateOperation.parseObject(
+        operationObject,
+        operationBuffer,
+        isAnchorFileMode
+      );
+    } else if (operationType === OperationType.Update) {
+      return UpdateOperation.parseObject(
+        operationObject,
+        operationBuffer,
+        isAnchorFileMode
+      );
+    } else if (operationType === OperationType.Recover) {
+      return RecoverOperation.parseObject(
+        operationObject,
+        operationBuffer,
+        isAnchorFileMode
+      );
+    } else if (operationType === OperationType.Deactivate) {
+      return DeactivateOperation.parseObject(
+        operationObject,
+        operationBuffer,
+        isAnchorFileMode
+      );
+    } else {
+      throw new SidetreeError(ErrorCode.OperationTypeUnknownOrMissing);
+    }
+  }
 
   /**
    * Parses the given encoded delta string into an internal `DeltaModel`.
