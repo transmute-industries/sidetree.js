2a21419fb4860fb7b2b0779e0e050974f18475fc
gjgd <gjgd@protonmail.com>
2020-06-09 13:21:31 +0200

    test: add BatchScheduler test

---
+++ packages/sidetree.js/src/test/write/BatchScheduler.spec.ts
@@ -0,0 +1,40 @@
+import retry from 'async-retry';
+import BatchScheduler from '../../write/BatchScheduler';
+import MockBatchWriter from '../mocks/MockBatchWriter';
+import MockBlockchain from '../mocks/MockBlockchain';
+import MockVersionManager from '../mocks/MockVersionManager';
+
+describe('BatchScheduler', () => {
+  it('should periodically invoke batch writer.', async () => {
+    const blockchain = new MockBlockchain();
+    const batchWriter = new MockBatchWriter();
+
+    const versionManager = new MockVersionManager();
+    spyOn(versionManager, 'getBatchWriter').and.returnValue(batchWriter);
+
+    const batchScheduler = new BatchScheduler(versionManager, blockchain, 1);
+
+    batchScheduler.startPeriodicBatchWriting();
+
+    // Monitor the Batch Scheduler until the Batch Writer is invoked or max retries is reached.
+    await retry(
+      async () => {
+        if (batchWriter.invocationCount >= 2) {
+          return;
+        }
+
+        // NOTE: if anything throws, we retry.
+        throw new Error('Batch writer not invoked.');
+      },
+      {
+        retries: 5,
+        minTimeout: 1000, // milliseconds
+        maxTimeout: 1000, // milliseconds
+      }
+    );
+
+    batchScheduler.stopPeriodicBatchWriting();
+
+    expect(batchWriter.invocationCount).toBeGreaterThanOrEqual(2);
+  });
+});
