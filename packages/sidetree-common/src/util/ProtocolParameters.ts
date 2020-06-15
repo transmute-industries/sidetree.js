53c2ec9d5122f96d8f76fc8f0593769ff2b24d02
gjgd <gjgd@protonmail.com>
2020-06-15 12:42:27 +0200

    feat: add index files to sidetree common

--- packages/sidetree-common/src/util/ProtocolParameters.ts
+++ packages/sidetree-common/src/util/ProtocolParameters.ts
@@ -3,6 +3,14 @@
 /**
  * Defines the list of protocol parameters, intended ONLY to be used within each version of Sidetree.
  */
-const protocolParameters: ProtocolParameters = require('./protocol-parameters.json');
-
+const protocolParameters: ProtocolParameters = {
+  hashAlgorithmInMultihashCode: 18,
+  maxAnchorFileSizeInBytes: 1000000,
+  maxMapFileSizeInBytes: 1000000,
+  maxChunkFileSizeInBytes: 20000000,
+  maxNumberOfOperationsPerTransactionTime: 600000,
+  maxNumberOfTransactionsPerTransactionTime: 300,
+  maxOperationsPerBatch: 10000,
+  maxDeltaSizeInBytes: 1000,
+};
 export default protocolParameters;
---
+++ packages/sidetree-common/src/util/index.ts
@@ -0,0 +1,15 @@
+import Encoder from './Encoder';
+import JsonCanonicalizer from './JsonCanonicalizer';
+import Multihash from './Multihash';
+import protocolParameters from './protocolParameters';
+import ReadableStream from './ReadableStream';
+import ServiceVersionFetcher from './ServiceVersionFetcher';
+
+export {
+  Encoder,
+  JsonCanonicalizer,
+  Multihash,
+  protocolParameters,
+  ReadableStream,
+  ServiceVersionFetcher,
+};
