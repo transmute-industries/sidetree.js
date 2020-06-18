572a26d4d80c75ad6b13a9cea8945e39bbea23ca
gjgd <gjgd@protonmail.com>
2020-06-18 17:44:56 +0200

    test: add missing test file

---
+++ packages/sidetree-ledger-ethereum/src/__tests__/EthereumBlockchain.test.ts
@@ -0,0 +1,15 @@
+import { LedgerEthereum } from '../LedgerEthereum';
+import testSuite from './testSuite';
+import { web3, logger } from '../__fixtures__';
+
+const ledger = new LedgerEthereum(
+  web3,
+  '0xeaf43D28235275afDB504aBF49863e778a4Cfea0',
+  logger
+);
+
+beforeAll(async () => {
+  await ledger._createNewContract();
+});
+
+testSuite(ledger);
