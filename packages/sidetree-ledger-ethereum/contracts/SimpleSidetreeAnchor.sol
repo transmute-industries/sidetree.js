e5f51a00ef2dbf1d9e8fe926da898ca5eb7000ad
gjgd <gjgd@protonmail.com>
2020-06-18 19:09:13 +0200

    feat: add indexed field to smart contract

--- packages/sidetree-ledger-ethereum/contracts/SimpleSidetreeAnchor.sol
+++ packages/sidetree-ledger-ethereum/contracts/SimpleSidetreeAnchor.sol
@@ -2,7 +2,7 @@ pragma solidity 0.5.0;
 
 contract SimpleSidetreeAnchor {
     uint256 public transactionNumber = 0;
-    event Anchor(bytes32 anchorFileHash, uint256 transactionNumber);
+    event Anchor(bytes32 anchorFileHash, uint256 indexed transactionNumber);
     function anchorHash(bytes32 _anchorHash) public {
         emit Anchor(_anchorHash, transactionNumber);
         transactionNumber = transactionNumber + 1;
