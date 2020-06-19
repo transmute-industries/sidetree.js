ebe34d169a91fddd367696c7d7868a57b68487f2
gjgd <gjgd@protonmail.com>
2020-06-19 13:01:03 +0200

    feat: add numberOfOperations param to smart contract

--- packages/sidetree-ledger-ethereum/contracts/SimpleSidetreeAnchor.sol
+++ packages/sidetree-ledger-ethereum/contracts/SimpleSidetreeAnchor.sol
@@ -2,9 +2,11 @@ pragma solidity 0.5.0;
 
 contract SimpleSidetreeAnchor {
     uint256 public transactionNumber = 0;
-    event Anchor(bytes32 anchorFileHash, uint256 indexed transactionNumber);
-    function anchorHash(bytes32 _anchorHash) public {
-        emit Anchor(_anchorHash, transactionNumber);
+
+    event Anchor(bytes32 anchorFileHash, uint256 indexed transactionNumber, uint numberOfOperations);
+
+    function anchorHash(bytes32 _anchorHash, uint _numberOfOperations) public {
+        emit Anchor(_anchorHash, transactionNumber, _numberOfOperations);
         transactionNumber = transactionNumber + 1;
     }
 }
