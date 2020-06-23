43f657eb37558ca06d263f199578d4d1f5b091e2
gjgd <gjgd@protonmail.com>
2020-06-23 14:30:35 +0200

    chore: update public key models

--- packages/common/src/enums/index.ts
+++ packages/common/src/enums/index.ts
@@ -1,11 +1,13 @@
 import FetchResultCode from './FetchResultCode';
 import OperationType from './OperationType';
+import PublicKeyPurpose from './PublicKeyPurpose';
 import PublicKeyUsage from './PublicKeyUsage';
 import TransactionProcessingStatus from './TransactionProcessingStatus';
 
 export {
   FetchResultCode,
   OperationType,
+  PublicKeyPurpose,
   PublicKeyUsage,
   TransactionProcessingStatus,
 };
