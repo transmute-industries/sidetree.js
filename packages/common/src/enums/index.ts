0014966bb291e71f387ecf9f3964c153771b1c13
gjgd <gjgd@protonmail.com>
2020-06-23 18:19:59 +0200

    chore(files): remove PublicKeyUsage

--- packages/common/src/enums/index.ts
+++ packages/common/src/enums/index.ts
@@ -1,13 +1,11 @@
 import FetchResultCode from './FetchResultCode';
 import OperationType from './OperationType';
 import PublicKeyPurpose from './PublicKeyPurpose';
-import PublicKeyUsage from './PublicKeyUsage';
 import TransactionProcessingStatus from './TransactionProcessingStatus';
 
 export {
   FetchResultCode,
   OperationType,
   PublicKeyPurpose,
-  PublicKeyUsage,
   TransactionProcessingStatus,
 };
