031f37c52214d7a6af188e236d063381fea8e7a8
gjgd <gjgd@protonmail.com>
2020-06-24 14:44:09 +0200

    feat: add Response + test

---
+++ packages/core/src/Response.ts
@@ -0,0 +1,23 @@
+import { ResponseStatus } from '@sidetree/common';
+
+/**
+ * Contains operations related to `IResponse`.
+ */
+export default class Response {
+  /**
+   * Converts a Sidetree response status to an HTTP status.
+   */
+  public static toHttpStatus(status: ResponseStatus): number {
+    switch (status) {
+      case ResponseStatus.Succeeded:
+        return 200;
+      case ResponseStatus.BadRequest:
+        return 400;
+      case ResponseStatus.NotFound:
+        return 404;
+      case ResponseStatus.ServerError:
+      default:
+        return 500;
+    }
+  }
+}
