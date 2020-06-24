1ec8553da4500b80a07ba72e6a804fea1ffd3a30
gjgd <gjgd@protonmail.com>
2020-06-24 15:54:41 +0200

    test: add Jwk test

---
+++ packages/core/src/test/util/Jwk.spec.ts
@@ -0,0 +1,78 @@
+import { ErrorCode, SidetreeError } from '@sidetree/common';
+import Jwk from '../../util/Jwk';
+
+describe('Jwk', () => {
+  describe('validateJwkEs256k()', () => {
+    it('should throw error if `undefined` is passed.', async () => {
+      expect(() => {
+        Jwk.validateJwkEs256k(undefined);
+      }).toThrow(new SidetreeError(ErrorCode.JwkEs256kUndefined));
+    });
+
+    it('should throw error if un unknown property is included in the JWK.', async () => {
+      const jwk = {
+        unknownProperty: 'any value',
+        kty: 'EC',
+        crv: 'secp256k1',
+        x: '5s3-bKjD1Eu_3NJu8pk7qIdOPl1GBzU_V8aR3xiacoM',
+        y: 'v0-Q5H3vcfAfQ4zsebJQvMrIg3pcsaJzRvuIYZ3_UOY',
+      };
+
+      expect(() => {
+        Jwk.validateJwkEs256k(jwk);
+      }).toThrow(new SidetreeError(ErrorCode.JwkEs256kHasUnknownProperty));
+    });
+
+    it('should throw error if JWK has the wrong `kty` value.', async () => {
+      const jwk = {
+        kty: 'WRONG_TYPE',
+        crv: 'secp256k1',
+        x: '5s3-bKjD1Eu_3NJu8pk7qIdOPl1GBzU_V8aR3xiacoM',
+        y: 'v0-Q5H3vcfAfQ4zsebJQvMrIg3pcsaJzRvuIYZ3_UOY',
+      };
+
+      expect(() => {
+        Jwk.validateJwkEs256k(jwk);
+      }).toThrow(new SidetreeError(ErrorCode.JwkEs256kMissingOrInvalidKty));
+    });
+
+    it('should throw error if JWK has the wrong `crv` value.', async () => {
+      const jwk = {
+        kty: 'EC',
+        crv: 'WRONG_CURVE',
+        x: '5s3-bKjD1Eu_3NJu8pk7qIdOPl1GBzU_V8aR3xiacoM',
+        y: 'v0-Q5H3vcfAfQ4zsebJQvMrIg3pcsaJzRvuIYZ3_UOY',
+      };
+
+      expect(() => {
+        Jwk.validateJwkEs256k(jwk);
+      }).toThrow(new SidetreeError(ErrorCode.JwkEs256kMissingOrInvalidCrv));
+    });
+
+    it('should throw error if JWK has the wrong `x` type.', async () => {
+      const jwk = {
+        kty: 'EC',
+        crv: 'secp256k1',
+        x: 123,
+        y: 'v0-Q5H3vcfAfQ4zsebJQvMrIg3pcsaJzRvuIYZ3_UOY',
+      };
+
+      expect(() => {
+        Jwk.validateJwkEs256k(jwk);
+      }).toThrow(new SidetreeError(ErrorCode.JwkEs256kMissingOrInvalidTypeX));
+    });
+
+    it('should throw error if JWK has the wrong `y` type.', async () => {
+      const jwk = {
+        kty: 'EC',
+        crv: 'secp256k1',
+        x: '5s3-bKjD1Eu_3NJu8pk7qIdOPl1GBzU_V8aR3xiacoM',
+        y: 123,
+      };
+
+      expect(() => {
+        Jwk.validateJwkEs256k(jwk);
+      }).toThrow(new SidetreeError(ErrorCode.JwkEs256kMissingOrInvalidTypeY));
+    });
+  });
+});
