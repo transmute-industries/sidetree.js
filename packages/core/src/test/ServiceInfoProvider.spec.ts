14dec22563eb45c36a9bdad1f10ee715df11281e
gjgd <gjgd@protonmail.com>
2020-06-29 20:02:19 +0200

    test: add tests for ServiceInfoProvider

---
+++ packages/core/src/test/ServiceInfoProvider.spec.ts
@@ -0,0 +1,13 @@
+import ServiceInfo from '../ServiceInfoProvider';
+
+describe('ServiceInfoProvider', () => {
+  it('should return the version from the package.json file.', async () => {
+    const packageJson = require('../../package.json');
+
+    const serviceInfo = new ServiceInfo('test-service');
+    const serviceVersion = serviceInfo.getServiceVersion();
+
+    expect(serviceVersion.name).toEqual('test-service');
+    expect(serviceVersion.version).toEqual(packageJson.version);
+  });
+});
