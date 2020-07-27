71043f5448521d5063ec6e7b9f1f2f48bdc11d78
GJGD <gjgd@protonmail.com>
2020-07-27 12:48:37 +0000

    chore: fix async test

--- packages/element/src/test/chunkMapAnchorFile.spec.ts
+++ packages/element/src/test/chunkMapAnchorFile.spec.ts
@@ -21,30 +21,36 @@ afterAll(async () => {
   await element.close();
 });
 
-it('should generate valid files', async done => {
+it('should generate valid files', async () => {
   expect.assertions(3);
   const spy = jest.spyOn((element as any).cas, 'write');
   let spyCallCounter = 0;
+  let chunkFileBuffer: Buffer;
+  let mapFileBuffer: Buffer;
+  let anchorFileBuffer: Buffer;
   spy.mockImplementation(async content => {
     const encodedHash = await MockCas.getAddress(content as Buffer);
     if (spyCallCounter === 0) {
       // Chunk file
-      const parsedChunkFile = await ChunkFile.parse(content as Buffer);
-      expect(parsedChunkFile).toEqual(createChunkFile);
+      chunkFileBuffer = content as Buffer;
     } else if (spyCallCounter === 1) {
       // Map file
-      const parsedMapFile = await MapFile.parse(content as Buffer);
-      expect(parsedMapFile).toEqual(createMapFile);
+      mapFileBuffer = content as Buffer;
     } else if (spyCallCounter === 2) {
       // Anchor file
-      const parsedAnchorFile = await AnchorFile.parse(content as Buffer);
-      const jsonAnchorFile = JSON.parse(JSON.stringify(parsedAnchorFile));
-      expect(jsonAnchorFile).toEqual(createAnchorFile);
-      done();
+      anchorFileBuffer = content as Buffer;
     }
     spyCallCounter += 1;
     return encodedHash;
   });
   await element.handleOperationRequest(createOperationBuffer);
   await element.triggerBatchWriting();
+
+  const parsedChunkFile = await ChunkFile.parse(chunkFileBuffer!);
+  expect(parsedChunkFile).toEqual(createChunkFile);
+  const parsedMapFile = await MapFile.parse(mapFileBuffer!);
+  expect(parsedMapFile).toEqual(createMapFile);
+  const parsedAnchorFile = await AnchorFile.parse(anchorFileBuffer!);
+  const jsonAnchorFile = JSON.parse(JSON.stringify(parsedAnchorFile));
+  expect(jsonAnchorFile).toEqual(createAnchorFile);
 });
