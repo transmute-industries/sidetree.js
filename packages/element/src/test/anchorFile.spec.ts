d46ed0067ce03e7ece890b0135c0a82ab1f7b225
GJGD <gjgd@protonmail.com>
2020-07-27 11:55:17 +0000

    test: add test for chunk file and map file

--- packages/element/src/test/anchorFile.spec.ts
+++ packages/element/src/test/anchorFile.spec.ts
@@ -1,8 +1,10 @@
 import { MockCas } from '@sidetree/cas';
-import { AnchorFile } from '@sidetree/core';
+import { AnchorFile, ChunkFile, MapFile } from '@sidetree/core';
 import Element from '../Element';
 import {
-  anchorFile,
+  createChunkFile,
+  createMapFile,
+  createAnchorFile,
   createOperationBuffer,
 } from './__fixtures__';
 import { getTestElement } from './utils';
@@ -19,17 +21,28 @@ afterAll(async () => {
   await element.close();
 });
 
-it('should generate a valid anchor file', async () => {
-  expect.assertions(1);
+it('should generate a valid anchor file', async (done) => {
+  expect.assertions(3);
   const spy = jest.spyOn((element as any).cas, 'write');
+  let spyCallCounter = 0;
   spy.mockImplementation(async (content) => {
-    try {
-      const parsedAnchorFile = await AnchorFile.parse(content as Buffer)
-      expect(parsedAnchorFile).toEqual(anchorFile);
-    } catch (e) {
-      // If we reach this point, the content was not an anchorFileBuffer
-    }
     const encodedHash = await MockCas.getAddress(content as Buffer);
+    if (spyCallCounter === 0) {
+      // Chunk file
+      const parsedChunkFile = await ChunkFile.parse(content as Buffer);
+      expect(parsedChunkFile).toEqual(createChunkFile);
+    } else if (spyCallCounter === 1) {
+      // Map file
+      const parsedMapFile = await MapFile.parse(content as Buffer);
+      expect(parsedMapFile).toEqual(createMapFile);
+    } else if (spyCallCounter === 2) {
+      // Anchor file
+      const parsedAnchorFile = await AnchorFile.parse(content as Buffer);
+      const jsonAnchorFile = JSON.parse(JSON.stringify(parsedAnchorFile))
+      expect(jsonAnchorFile).toEqual(createAnchorFile);
+      done();
+    }
+    spyCallCounter += 1;
     return encodedHash;
   });
   await element.handleOperationRequest(createOperationBuffer);
