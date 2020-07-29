78f345fcb6e7154858aeec4eef91e929d99a4c53
GJGD <gjgd@protonmail.com>
2020-07-29 12:51:44 +0000

    test: add test for converting key types

--- packages/element/src/test/did-key-interop.spec.ts
+++ packages/element/src/test/did-key-interop.spec.ts
@@ -7,24 +7,72 @@ const msg = Buffer.from(
   'e0cb3d2aa10da7445e52e7131c837ffc8281f0246ceb9d8f3a408d181824724f',
   'hex'
 );
-const privKeyHex = Buffer.from(
+const privKeyBuffer = Buffer.from(
   '77d5b3ac2c9bf0f11fe3eca90102f2a2adcf5285f2e0fc4b936dae17b33fece5',
   'hex'
 );
-const pubKeyHex = secp256k1.publicKeyCreate(privKeyHex);
+const pubKeyBuffer = secp256k1.publicKeyCreate(privKeyBuffer);
 
-const privKeyJwk = keyto.from(privKeyHex, 'blk').toJwk('private');
-privKeyJwk.crv = 'secp256k1';
+const privKeyJwk = {
+  kty: 'EC',
+  crv: 'secp256k1',
+  d: 'd9WzrCyb8PEf4-ypAQLyoq3PUoXy4PxLk22uF7M_7OU',
+  x: '3PAeFnNa_R6vxH6dDfzoH5K0O7UAmX342SzlL-4aEZE',
+  y: '1Rw6RknAvKUDaRFVO_NlTJnp7PrxtBfQOvlScUIk6qY',
+};
 
-const publicKeyJwk = keyto.from(privKeyHex, 'blk').toJwk('public');
-publicKeyJwk.crv = 'secp256k1';
+const publicKeyJwk = {
+  kty: 'EC',
+  crv: 'secp256k1',
+  x: '3PAeFnNa_R6vxH6dDfzoH5K0O7UAmX342SzlL-4aEZE',
+  y: '1Rw6RknAvKUDaRFVO_NlTJnp7PrxtBfQOvlScUIk6qY',
+};
+
+it('should convert hex keys to jwk', async () => {
+  const generatedPrivKeyJwk = keyto.from(privKeyBuffer, 'blk').toJwk('private');
+  generatedPrivKeyJwk.crv = 'secp256k1';
+  expect(generatedPrivKeyJwk).toEqual(privKeyJwk);
+
+  const generatedPublicKeyJwk = keyto
+    .from(privKeyBuffer, 'blk')
+    .toJwk('public');
+  generatedPublicKeyJwk.crv = 'secp256k1';
+  expect(generatedPublicKeyJwk).toEqual(publicKeyJwk);
+});
+
+it('should convert jwk to hex keys', async () => {
+  const generatedPrivKeyBuffer = keyto
+    .from(
+      {
+        ...privKeyJwk,
+        crv: 'K-256',
+      },
+      'jwk'
+    )
+    .toString('blk', 'private');
+  expect(generatedPrivKeyBuffer).toEqual(privKeyBuffer.toString('hex'));
+
+  const generatedPubKeyHex = keyto
+    .from(
+      {
+        ...publicKeyJwk,
+        crv: 'K-256',
+      },
+      'jwk'
+    )
+    .toString('blk', 'public');
+  const pubKeyHex = Buffer.from(pubKeyBuffer.buffer).toString('hex');
+  expect(generatedPubKeyHex).toContain(pubKeyHex.slice(2));
+});
 
 it('should deterministically sign a message with the secp256k1 library', async () => {
-  const sigObj = secp256k1.ecdsaSign(msg, privKeyHex);
+  const sigObj = secp256k1.ecdsaSign(msg, privKeyBuffer);
   expect(sigObj.signature.toString()).toEqual(
     '93,1,81,185,221,191,130,146,188,160,194,22,68,101,164,242,90,169,53,35,56,104,146,45,244,150,85,218,189,87,7,90,103,239,178,217,186,78,14,211,238,94,127,188,200,233,106,104,100,234,77,181,63,176,214,202,76,142,127,204,43,143,11,133'
   );
-  expect(secp256k1.ecdsaVerify(sigObj.signature, msg, pubKeyHex)).toBeTruthy();
+  expect(
+    secp256k1.ecdsaVerify(sigObj.signature, msg, pubKeyBuffer)
+  ).toBeTruthy();
 });
 
 it('should nondeterministically sign a message with the JOSE library', async () => {
@@ -80,7 +128,7 @@ it('should sign with secp256k1 and verify with JOSE library', async () => {
   expect(verify).toBeTruthy();
 
   // Verify that secp256k1 signature can be verified by the JWS lib
-  const sigObj = secp256k1.ecdsaSign(msg, privKeyHex);
+  const sigObj = secp256k1.ecdsaSign(msg, privKeyBuffer);
   const sigBuffer = Buffer.from(sigObj.signature.buffer);
 
   const verify2 = await Jws.verifySignature(
