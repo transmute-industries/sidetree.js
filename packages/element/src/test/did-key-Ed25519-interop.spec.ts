f9c48c4ad8eda86252c4f23e99bf1b709da1eeb8
gjgd <gjgd@protonmail.com>
2020-07-31 13:07:37 +0200

    test: add test for jose library

--- packages/element/src/test/did-key-Ed25519-interop.spec.ts
+++ packages/element/src/test/did-key-Ed25519-interop.spec.ts
@@ -1,6 +1,5 @@
 import { EdDSA } from '@transmute/did-key-ed25519';
-import { Jwk } from '@sidetree/core';
-// import { JWS } from 'jose';
+import { JWS } from 'jose';
 
 const msg = Buffer.from(JSON.stringify({ hello: 'world' }));
 
@@ -8,10 +7,22 @@ const header = {
   alg: 'EdDSA',
 };
 
+const publicKey = {
+  crv: 'Ed25519',
+  x: 'h18Gznm3RaMjA-scDPv1FX53HwdxnHZ1_-XMuACNocg',
+  kty: 'OKP',
+  kid: 'rEJj4jyDHV30s0JBeVFLEthaBpj-1qzd51-sCIjw1L8',
+};
+const privateKey = {
+  crv: 'Ed25519',
+  x: 'h18Gznm3RaMjA-scDPv1FX53HwdxnHZ1_-XMuACNocg',
+  d: 'MfITzL3pEG3XZwYcyWAtqi3edbpaDQDEVeOpn3avXdE',
+  kty: 'OKP',
+  kid: 'rEJj4jyDHV30s0JBeVFLEthaBpj-1qzd51-sCIjw1L8',
+};
+
 describe('Interop between did key Ed25519 lib and JOSE lib', () => {
   it('should deterministically sign a message with did-key Ed25519 lib', async () => {
-    const [publicKey, privateKey] = await Jwk.generateEd25519KeyPair();
-
     const jws = await EdDSA.sign(msg, privateKey, header);
     const verified = await EdDSA.verify(jws, publicKey);
     expect(verified).toBeTruthy();
@@ -23,20 +34,18 @@ describe('Interop between did key Ed25519 lib and JOSE lib', () => {
     expect(jws).toEqual(jws2);
   });
 
-  // it('should nondeterministically sign a message with the JOSE library', async () => {
-  //   const jws = await JWS.sign(msg, privateKeyJwk as any, header);
-  //   const verified = await JWS.verify(jws, publicKeyJwk as any);
-  //   expect(verified).toBeTruthy();
-  //   const jws2 = await JWS.sign(msg, privateKeyJwk as any, header);
-  //   const verified2 = await JWS.verify(jws2, publicKeyJwk as any);
-  //   expect(verified2).toBeTruthy();
-  //   // Show that signatures are different
-  //   const [protectedHeader, payload, signature] = jws.split('.');
-  //   const [protectedHeader2, payload2, signature2] = jws2.split('.');
-  //   expect(protectedHeader).toBe(protectedHeader2);
-  //   expect(payload).toBe(payload2);
-  //   expect(signature).not.toBe(signature2);
-  // });
+  it('should deterministically sign a message with the JOSE library', async () => {
+    const jws = await JWS.sign(msg, privateKey as any, header);
+    const verified = await JWS.verify(jws, publicKey as any);
+    expect(verified).toBeTruthy();
+
+    const jws2 = await JWS.sign(msg, privateKey as any, header);
+    const verified2 = await JWS.verify(jws2, publicKey as any);
+    expect(verified2).toBeTruthy();
+
+    // Signatures are the same because signature is deterministic
+    expect(jws).toEqual(jws2);
+  });
 
   // it('should sign with did key ES256K and verify with JOSE', async () => {
   //   const privateKeyWithKid = {
