63f4ac7711589a6a5266a2592e00c38e0eda33aa
gjgd <gjgd@protonmail.com>
2020-07-31 13:09:11 +0200

    test: add interop tests

--- packages/element/src/test/did-key-Ed25519-interop.spec.ts
+++ packages/element/src/test/did-key-Ed25519-interop.spec.ts
@@ -47,31 +47,15 @@ describe('Interop between did key Ed25519 lib and JOSE lib', () => {
     expect(jws).toEqual(jws2);
   });
 
-  // it('should sign with did key ES256K and verify with JOSE', async () => {
-  //   const privateKeyWithKid = {
-  //     ...privateKeyJwk,
-  //     kid: '',
-  //   };
-  //   const jws = await ES256K.sign(msg, privateKeyWithKid);
-  //   const verified = await JWS.verify(jws, publicKeyJwk as any);
-  //   expect(verified).toBeTruthy();
-  // });
+  it('should sign with did key Ed25519 and verify with JOSE', async () => {
+    const jws = await EdDSA.sign(msg, privateKey);
+    const verified = await JWS.verify(jws, publicKey as any);
+    expect(verified).toBeTruthy();
+  });
 
-  // it('should sign with JOSE and verify with did key ES256K ', async () => {
-  //   const jws = await JWS.sign(msg, privateKeyJwk as any, header);
-  //   const publicKeyWithKid = {
-  //     ...publicKeyJwk,
-  //     kid: '',
-  //   };
-  //   try {
-  //     const verified = await ES256K.verify(jws, publicKeyWithKid);
-  //     expect(verified).toBeTruthy();
-  //   } catch (e) {
-  //     // 50% of the time this check will fail and 50% of the time it will work
-  //     // It is probably because of the k value being chosen at random in JOSE
-  //     // See https://crypto.stackexchange.com/questions/851/can-ecdsa-signatures-be-safely-made-deterministic
-  //     // However the other way around (see test above) works 100% of the time
-  //     console.log(e);
-  //   }
-  // });
+  it('should sign with JOSE and verify with did key Ed25519 ', async () => {
+    const jws = await JWS.sign(msg, privateKey as any, header);
+    const verified = await EdDSA.verify(jws, publicKey);
+    expect(verified).toBeTruthy();
+  });
 });
