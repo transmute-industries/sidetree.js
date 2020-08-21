44a91f2c2e2fd56fbf1ba599a98fb581454409d8
GJGD <gjgd@protonmail.com>
2020-08-21 11:30:48 +0000

    chore: restore chunkMapAnchorFile test

--- packages/element/src/test/chunkMapAnchorFile.spec.ts
+++ packages/element/src/test/chunkMapAnchorFile.spec.ts
@@ -1,13 +1,15 @@
 import { MockCas } from '@sidetree/cas';
 import { AnchorFile, ChunkFile, MapFile } from '@sidetree/core';
 import Element from '../Element';
-import {
+import { generatedTestVectors } from '@sidetree/test-vectors';
+import { getTestElement } from './utils';
+
+const {
   createChunkFile,
   createMapFile,
   createAnchorFile,
   createOperationBuffer,
-} from './__fixtures__';
-import { getTestElement } from './utils';
+} = generatedTestVectors;
 
 console.info = () => null;
 
@@ -22,13 +24,13 @@ afterAll(async () => {
 });
 
 // move these tests to test-vector-conformance.spec.ts
-it.skip('create operation should generate expected chunk map and anchor files', async () => {
+it('create operation should generate expected chunk map and anchor files', async () => {
   const spy = jest.spyOn((element as any).cas, 'write');
   let spyCallCounter = 0;
   let chunkFileBuffer: Buffer;
   let mapFileBuffer: Buffer;
   let anchorFileBuffer: Buffer;
-  spy.mockImplementation(async (content) => {
+  spy.mockImplementation(async content => {
     const encodedHash = await MockCas.getAddress(content as Buffer);
     if (spyCallCounter === 0) {
       // Chunk file
