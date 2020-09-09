e351aa64706ec879b51608b6af537575377c8b14
Orie Steele <orie@transmute.industries>
2020-09-09 15:39:14 -0500

    chore: lint

--- packages/test-vectors/src/index.ts
+++ packages/test-vectors/src/index.ts
@@ -1,6 +1,6 @@
-export { testVectors } from './dif-sidetree-test-vectors';
-
 import * as filesystem from './filesystem';
 import * as wallet from './wallet';
 
 export { filesystem, wallet };
+
+export { testVectors } from './dif-sidetree-test-vectors';
