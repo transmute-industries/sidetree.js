1bd83dcfdaa9370f4d0bd89e4fd3e72a6d170cba
GJGD <gjgd@protonmail.com>
2020-09-17 16:42:54 +0000

    chore: add types to ethereum package

--- packages/ethereum/src/utils.ts
+++ packages/ethereum/src/utils.ts
@@ -2,10 +2,12 @@ import { TransactionModel, AnchoredDataSerializer } from '@sidetree/common';
 import multihashes from 'multihashes';
 import Web3 from 'web3';
 import { BlockTransactionString } from 'web3-eth';
+import { EventData } from 'web3-eth-contract';
+import BN from 'bn.js';
 
 const getAccounts = (web3: Web3): Promise<Array<string>> =>
   new Promise((resolve, reject) => {
-    web3.eth.getAccounts((err: any, accounts: any) => {
+    web3.eth.getAccounts((err: Error, accounts: string[]) => {
       if (err) {
         reject(err);
       }
@@ -15,20 +17,31 @@ const getAccounts = (web3: Web3): Promise<Array<string>> =>
 
 const bytes32EnodedMultihashToBase58EncodedMultihash = (
   bytes32EncodedMultihash: string
-) =>
+): string =>
   multihashes.toB58String(
     multihashes.fromHexString(
       `1220${bytes32EncodedMultihash.replace('0x', '')}`
     )
   );
 
-const base58EncodedMultihashToBytes32 = (base58EncodedMultihash: string) => {
+const base58EncodedMultihashToBytes32 = (
+  base58EncodedMultihash: string
+): string => {
   return `0x${multihashes
     .toHexString(multihashes.fromB58String(base58EncodedMultihash))
     .substring(4)}`;
 };
 
-const eventLogToSidetreeTransaction = (log: any) => {
+interface ElementEventData extends EventData {
+  args: {
+    anchorFileHash: string;
+    numberOfOperations: string;
+    transactionNumber: BN;
+  };
+}
+const eventLogToSidetreeTransaction = (
+  log: ElementEventData
+): TransactionModel => {
   const anchoredData = {
     anchorFileHash: bytes32EnodedMultihashToBase58EncodedMultihash(
       log.args.anchorFileHash
@@ -40,15 +53,16 @@ const eventLogToSidetreeTransaction = (log: any) => {
     transactionNumber: log.args.transactionNumber.toNumber(),
     transactionTime: log.blockNumber,
     transactionTimeHash: log.blockHash,
-    transactionHash: log.transactionHash,
     anchorString,
+    transactionFeePaid: 0,
+    normalizedTransactionFee: 0,
     writer: 'writer',
   };
 };
 
 const getBlock = async (
   web3: Web3,
-  blockHashOrBlockNumber: string
+  blockHashOrBlockNumber: string | number
 ): Promise<BlockTransactionString> => {
   const block: BlockTransactionString = await new Promise((resolve, reject) => {
     web3.eth.getBlock(
@@ -66,7 +80,7 @@ const getBlock = async (
 
 const getBlockchainTime = async (
   web3: Web3,
-  blockHashOrBlockNumber: string
+  blockHashOrBlockNumber: string | number
 ): Promise<string | number | null> => {
   const block: BlockTransactionString = await getBlock(
     web3,
@@ -80,15 +94,18 @@ const getBlockchainTime = async (
 
 const extendSidetreeTransactionWithTimestamp = async (
   web3: Web3,
-  txns: any
+  txns: TransactionModel[]
 ): Promise<TransactionModel[]> => {
   return Promise.all(
-    txns.map(async (txn: any) => {
+    txns.map(async txn => {
       const timestamp = await getBlockchainTime(web3, txn.transactionTime);
-      return {
-        ...txn,
-        transactionTimestamp: timestamp,
-      };
+      if (typeof timestamp === 'number') {
+        return {
+          ...txn,
+          transactionTimestamp: timestamp,
+        };
+      }
+      return txn;
     })
   );
 };
