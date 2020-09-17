a1cb6d67cdc683d048dbbd5777b955ae19d24c6f
GJGD <gjgd@protonmail.com>
2020-09-17 09:04:11 +0000

    feat: export all operation types from core

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,12 +2,15 @@ import AnchorFile from './write/AnchorFile';
 import BatchScheduler from './write/BatchScheduler';
 import ChunkFile from './write/ChunkFile';
 import CreateOperation from './CreateOperation';
+import DeactivateOperation from './DeactivateOperation';
 import DownloadManager from './DownloadManager';
 import Jwk from './util/Jwk';
 import Jws from './util/Jws';
 import MapFile from './write/MapFile';
 import Observer from './Observer';
+import Operation from './Operation';
 import OperationGenerator from './test/generators/OperationGenerator';
+import RecoverOperation from './RecoverOperation';
 import Resolver from './Resolver';
 import ServiceInfo from './ServiceInfoProvider';
 import UpdateOperation from './UpdateOperation';
@@ -18,12 +21,15 @@ export {
   BatchScheduler,
   ChunkFile,
   CreateOperation,
+  DeactivateOperation,
   DownloadManager,
   Jwk,
   Jws,
   MapFile,
   Observer,
+  Operation,
   OperationGenerator,
+  RecoverOperation,
   Resolver,
   ServiceInfo,
   UpdateOperation,
