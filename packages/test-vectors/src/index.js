dbd6ba730cb5e542296add583aba3a0213f134d4
gjgd <gjgd@protonmail.com>
2020-09-23 11:28:51 +0000

    chore: remove wallet test vectors

--- packages/test-vectors/src/index.js
+++ packages/test-vectors/src/index.js
@@ -1,17 +1,15 @@
 const testVectors = require('./dif-sidetree-test-vectors');
-const wallet = require('./wallet');
 const filesystem = require('./filesystem');
-
 const crypto = require('./crypto');
 const sidetreeUniversalWallet = require('./sidetree-universal-wallet');
 const sidetreeCoreGeneratedSecp256k1 = require('./core-generated-secp256k1');
 const sidetreeCoreGeneratedEd25519 = require('./core-generated-ed25519');
+
 module.exports = {
-  testVectors,
-  wallet,
-  filesystem,
-  crypto,
-  sidetreeUniversalWallet,
-  sidetreeCoreGeneratedSecp256k1,
-  sidetreeCoreGeneratedEd25519,
+  testVectors, // OK
+  filesystem, // OK
+  crypto, // OK
+  sidetreeUniversalWallet, // Ok
+  sidetreeCoreGeneratedSecp256k1, // OK
+  sidetreeCoreGeneratedEd25519, // OK
 };
--- packages/test-vectors/src/wallet/index.js
+++
@@ -1,6 +0,0 @@
-const mnemonic = require('./mnemonic.json');
-const keypair = require('./keypair.json');
-const did = require('./did.json');
-const sidetree = require('./sidetree.json');
-
-module.exports = { mnemonic, keypair, did, sidetree };
