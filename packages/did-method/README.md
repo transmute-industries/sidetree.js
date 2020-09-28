603671b199f58adf0f92b3be6c1a572e8005649b
gjgd <gjgd@protonmail.com>
2020-09-28 08:06:33 +0000

    feat: add did-method package

---
+++ packages/did-method/README.md
@@ -0,0 +1,14 @@
+# @sidetree/did-method
+
+## Usage
+
+```
+npm install --save @sidetree/did-method
+```
+
+## Development
+
+```
+npm install
+npm run test
+```
---
+++ packages/did-method/src/typings.d.ts
@@ -0,0 +1,6 @@
+// declare module 'ipfs-unixfs';
+// declare module 'ipld-dag-pb';
+// declare module 'ipfs-http-client';
+// declare module 'canonicalize';
+// declare module 'multihashes';
+//
