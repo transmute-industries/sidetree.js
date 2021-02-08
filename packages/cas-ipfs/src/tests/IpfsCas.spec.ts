339416b4375d299d75645b0abb010f73d3b9a64e
gjgd <gjgd@protonmail.com>
2021-02-08 03:19:10 +0000

    chore: debug ci

--- packages/cas-ipfs/src/tests/IpfsCas.spec.ts
+++ packages/cas-ipfs/src/tests/IpfsCas.spec.ts
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 it('should work', async () => {
   const data = await fetch(
-    'http://127.0.0.1:5001/api/v0/get?timeout=2000ms&arg=QmNrEidQrAbxx3FzxNt9E6qjEDZrtvzxUVh47BXm55Zuen',
+    'http://localhost:5001/api/v0/get?timeout=2000ms&arg=QmNrEidQrAbxx3FzxNt9E6qjEDZrtvzxUVh47BXm55Zuen',
     { method: 'POST' }
   ).then((res: any) => res.text());
   console.log(data);
