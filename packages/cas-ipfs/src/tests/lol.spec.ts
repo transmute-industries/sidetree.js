ca0821b01e553d4c188646e402efa7ccc299cf6c
gjgd <gjgd@protonmail.com>
2021-02-15 02:57:21 +0000

    chore: debug ci

--- packages/cas-ipfs/src/tests/lol.spec.ts
+++
@@ -1,9 +0,0 @@
-import fetch from 'node-fetch';
-
-(async () => {
-  const data = await fetch(
-    'http://localhost:5001/api/v0/get?timeout=2000ms&arg=QmNrEidQrAbxx3FzxNt9E6qjEDZrtvzxUVh47BXm55Zuen',
-    { method: 'POST' }
-  ).then((res: any) => res.text());
-  console.log(data);
-})();
