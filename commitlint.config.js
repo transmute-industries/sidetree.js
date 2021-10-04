1996a647f54aa449cb8d2b84cddf3365a0c7f380
Orie Steele <orie@transmute.industries>
2021-10-04 15:57:27 -0500

    docs(core): update readme

--- commitlint.config.js
+++ commitlint.config.js
@@ -23,18 +23,19 @@ module.exports = {
     'type-enum': [
       2,
       'always',
+      // see https://github.com/conventional-changelog/commitlint/blob/99d8881d0d951deded6d9e31bbb279d04101549b/%40commitlint/config-conventional/index.js#L38
       [
-        'build',
-        'chore',
-        'ci',
-        'docs',
-        'feat',
-        'fix',
-        'perf',
-        'refactor',
-        'revert',
-        'style',
-        'test',
+        'build', // Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)
+        'chore', // Other changes that don't modify src or test files
+        'ci', // Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)
+        'docs', // Documentation only changes
+        'feat', // A new feature
+        'fix', // A bug fix
+        'perf', // A code change that improves performance
+        'refactor', // A code change that neither fixes a bug nor adds a feature
+        'revert', // Reverts a previous commit
+        'style', // Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+        'test', // Adding missing tests or correcting existing tests
       ],
     ],
   },
