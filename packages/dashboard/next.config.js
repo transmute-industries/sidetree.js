b6e2c53aa264ffae6292f1b2da7ba924d76d3cc8
Adam Carnagey <adam.carnagey@transmute.industries>
2021-11-30 12:30:09 -0600

    chore(element): ignore electron module on nextjs server

--- packages/dashboard/next.config.js
+++ packages/dashboard/next.config.js
@@ -8,16 +8,26 @@ module.exports = {
   assetPrefix: isProd ? '' : '',
   // https://nextjs.org/docs/messages/import-esm-externals
   experimental: { esmExternals: 'loose', outputFileTracing: true },
-  webpack5: false,
   // see https://github.com/vercel/vercel/issues/2569#issuecomment-514865342
   webpack(config, { isServer }) {
     if (isServer) {
       config.resolve.mainFields = ['module', 'main'];
-
       // Fix all packages that this change breaks:
       config.resolve.alias['node-fetch'] = 'node-fetch/lib/index.js';
+      // if (!dev) {
+      const [nextJsExternals] = config.externals;
+      config.externals = [
+        (context, request, callback) => {
+          // Ignore electron, it is referenced in cas-ipfs/node_modules/electron-fetch
+          const IGNORES = ['electron'];
+          if (IGNORES.indexOf(request) >= 0) {
+            return callback(null, "require('" + request + "')");
+          }
+          nextJsExternals(context, request, callback);
+        },
+      ];
+      // }
     }
-
     return config;
   },
 };
