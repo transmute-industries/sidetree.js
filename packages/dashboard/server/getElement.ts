0d806aa5b1faeb1c444e50a9f866be714302d191
Adam Carnagey <adam.carnagey@transmute.industries>
2021-12-01 12:41:09 -0600

    chore(element): fix element constructor args

--- packages/dashboard/server/getElement.ts
+++ packages/dashboard/server/getElement.ts
@@ -2,6 +2,8 @@ import { Element } from '@sidetree/element';
 import { EthereumLedger } from '@sidetree/ethereum';
 import Web3 from 'web3';
 import { IpfsCasWithCache } from '@sidetree/cas-ipfs';
+// import { MockCas } from '@sidetree/cas';
+
 import config from '../config/test.json';
 
 const getTestLedger = async () => {
@@ -15,19 +17,19 @@ const getTestCas = async () => {
     config.contentAddressableStoreServiceUri,
     config.mongoDbConnectionString + config.databaseName
   );
+  // const cas = new MockCas();
   return cas;
 };
 
 async function createElement() {
   const ledger: any = await getTestLedger();
   const cas: any = await getTestCas();
-  const element = new Element(config as any, config.versions, ledger, cas);
-  await element.initialize(
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    { info: () => {}, warn: () => {}, error: () => {} },
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    { emit: (() => {}) as any }
-  );
+  const element = new Element(config as any, config.versions, cas, ledger);
+  await element.initialize();
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  // { info: () => {}, warn: () => {}, error: () => {} },
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  // { emit: (() => {}) as any }
   return element;
 }
 
