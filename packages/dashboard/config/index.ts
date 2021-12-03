7b557b150a935943bd890e8cd687ae581ca49959
Isaac Byrne <isaac.byrne@rocketinsights.com>
2021-12-03 16:03:19 -0600

    fix(core): adding config env variables

--- packages/dashboard/config/index.ts
+++ packages/dashboard/config/index.ts
@@ -1,12 +1,18 @@
 // todo: make this a single config for the entire node dashboard
 // including branding options, logos, etc.
-export const config = {
-  operator: 'Transmute',
-  method: 'Element',
-  description: 'Sidetree on Ethereum and IPFS.',
-  methodName: process.env.SIDETREE_METHOD || 'example:sidetree.testnet',
+export const config: any = {
+  operator: process.env.NEXT_PUBLIC_OPERATOR,
+  method: process.env.NEXT_PUBLIC_METHOD,
+  description: process.env.NEXT_PUBLIC_DESCRIPTION,
+  methodName: process.env.NEXT_PUBLIC_SIDETREE_METHOD,
   features: {
     title: 'Features',
     description: 'Decentralized Identifiers powered by Ethereum and IPFS.',
   },
+  connectionString: process.env.CONNECTION_STRING,
+  ethereumMnemonic: process.env.ETHEREUMMNEMONIC,
 };
+
+// When logged on the server, the non NEXT_PUBLIC_ variables values are readable.
+// When logged on the client, they are not
+console.log('CONFIG', config);
