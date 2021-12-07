9f61efccc3714b9e9c9112a073b830e894d214b4
Adam Carnagey <adam.carnagey@transmute.industries>
2021-12-07 13:14:57 -0600

    chore(element): env refactor

--- packages/dashboard/README.md
+++ packages/dashboard/README.md
@@ -4,10 +4,12 @@ This package is work in progress.
 
 This package contains a Next.js app for sidetree nodes.
 
+## Running the dev environment
+
+You'll need an .env file, you can copy the .env.example file to .env so that when you run `npm run dev` or `npm run dev:docker`
+it has the environment variable it needs.
+
 ### Building with Docker
-
-This command does not work yet, need to address package manager differences (yarn vs npm)
-
 ```
 docker-compose up
 ```
--- packages/dashboard/config/index.ts
+++ packages/dashboard/config/index.ts
@@ -2,10 +2,7 @@
 // including branding options, logos, etc.
 export const config: any = {
   operator: process.env.NEXT_PUBLIC_OPERATOR,
-  method:
-    process.env.NEXT_PUBLIC_USE_ELEMENT !== 'true'
-      ? process.env.NEXT_PUBLIC_SIDETREE_METHOD
-      : process.env.NEXT_PUBLIC_ELEMENT_METHOD,
+  method: process.env.NEXT_PUBLIC_SIDETREE_METHOD,
   description: process.env.NEXT_PUBLIC_DESCRIPTION,
   batchingIntervalInSeconds: process.env.BATCH_INTERVAL_IN_SECONDS,
   observingIntervalInSeconds: process.env.OBSERVING_INTERVAL_IN_SECONDS,
@@ -13,7 +10,6 @@ export const config: any = {
   databaseName: process.env.DATABASE_NAME,
   maxConcurrentDownloads: process.env.MAX_CONCURRENT_DOWNLOADS,
   port: process.env.SIDE_TREE_NODE_PORT,
-  useSideTreeNodeConfigs: process.env.NEXT_PUBLIC_USE_ELEMENT !== 'true',
   contentAddressableStoreServiceUri:
     process.env.ELEMENT_CONTENT_ADDRESSABLE_STORE_SERVICE_URI,
   ethereumRpcUrl: process.env.ETHEREUM_RCP_URL,
